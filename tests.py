f0950d38ae36ffcf78090e1df0feaf0f69d9381d
Justin Noah <justinnoah@gmail.com>
2014-09-20 15:07:37 -0700

    inital test that index is returning a 200

--- tests.py
+++ tests.py
@@ -1,9 +1,21 @@
 import unittest
+import urllib2
 
-from flask.ext.testing import TestCase
+from flask.ext.testing import LiveServerTestCase, TestCase
 
-class TestTMR(TestCase):
-    pass
+from tmr import app as tmrapp
+
+class TestIndex(LiveServerTestCase):
+
+    def create_app(self):
+        app = tmrapp
+        app.config['TESTING'] = True
+        app.config['LIVESERVER_PORT'] = 8943
+        return app
+
+    def test_server_is_up_and_running(self):
+        response = urllib2.urlopen(self.get_server_url())
+        self.assertEqual(response.code, 200)
 
 if __name__ == '__main__':
     unittest.main()

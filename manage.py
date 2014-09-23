8f50354013e1bbc265803e5f4e1dd6cd0c8334cf
Justin Noah <justinnoah@gmail.com>
2014-09-22 19:11:13 -0700

    move to a manage.py file
    start server with 'runserver'
    initialize the db with 'initdb'
    
    refs #2

--- manage.py
+++ manage.py
@@ -1,3 +1,12 @@
-from tmb import app
+from flask.ext.script import Manager
 
-app.run(debug=True)
+from tmb import app, db
+
+manager = Manager(app)
+
+@manager.command
+def initdb():
+    db.create_all()
+
+if __name__ == '__main__':
+    manager.run()
